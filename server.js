159ec42d997b0cba5bcbb4040193df120980cd0f
Eric Furlan <erick.furlan@hotmail.com>
2025-07-22 16:46:01 -0300

    Corrigir erro local e 404/JSON inválido na Vercel

--- server.js
+++ server.js
@@ -34,4 +34,10 @@ app.post('/send-sms', async (req, res) => {
   }
 });
 
+// Iniciar o servidor localmente
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
 module.exports = app;
